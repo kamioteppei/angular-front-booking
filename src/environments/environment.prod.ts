c212baef3e4780fbf20fb691ad949d4017f10960
kamioteppei <kamioteppei@gmail.com>
2019-03-06 21:25:25 +0900

    add favicon.ico

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   API_USER_AUTH_ENTRY_POINT_URL: 'http://kamioteppei-aws.ml/',
   API_DOMAIN_ENTRY_POINT_URL: 'http://kamioteppei-aws.ml/api/v1/',
+  // API_USER_AUTH_ENTRY_POINT_URL: 'http://spring-api-booking.kamioteppei-aws.ml/',
+  // API_DOMAIN_ENTRY_POINT_URL: 'http://spring-api-booking.kamioteppei-aws.ml/api/v1/',
 };
